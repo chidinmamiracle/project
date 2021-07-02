1dabe7057a7e0805b92565abfa5ebfc99c2a6495
chidinmamiracle <82915192+chidinmamiracle@users.noreply.github.com>
2021-07-02 12:59:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function operator(val) {
+  alert(params);
+  document.getElementById('result').value += val;
+}
 
+function clear() {
+  alert('i clicked clear');
+  document.getElementById('result').value = '';
+}
+
+function number(val) {
+  alert(params);
+  document.getElementById('result').value += val;
+}
+
+function equal() {
+  alert('i clicked equal');
+  let x = document.getElementById('result').value;
+  let y = eval[x];
+  document.getElementById('result').value = y;
+}
+
+function decimal() {
+  alert('i clicked decimal');
+  document.getElementById('result').value = '';
+}
